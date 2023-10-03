5324975854b189e00ed76e6ed0ea8618218550de
Anna <git@anna.lgbt>
2023-10-03 17:34:10 -0400

    chore: update XivCommon

--- ChatTwo/Plugin.cs
+++ ChatTwo/Plugin.cs
@@ -86,8 +86,8 @@ public sealed class Plugin : IDalamudPlugin {
         this.LanguageChanged(this.Interface.UiLanguage);
 
         this.Commands = new Commands(this);
-        this.Common = new XivCommonBase();
-        this.TextureCache = new TextureCache(this.DataManager!);
+        this.Common = new XivCommonBase(this.Interface);
+        this.TextureCache = new TextureCache(this.TextureProvider!);
         this.Functions = new GameFunctions.GameFunctions(this);
         this.Store = new Store(this);
         this.Ipc = new IpcManager(this.Interface);
